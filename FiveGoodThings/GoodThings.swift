278a446b5bcc80e56ffdacc63ccc2bd14fe975aa
Cody Rice <cody.rice@gmail.com>
2017-07-26 09:47:38 +0200

    Testing new repo

---
+++ FiveGoodThings/GoodThings.swift
@@ -0,0 +1,66 @@
+//
+//  GoodThings.swift
+//  fiveGoodThings
+//
+//  Created by Devin Rice on 7/25/17.
+//  Copyright © 2017 Devin Rice. All rights reserved.
+//
+
+import Foundation
+import os.log
+
+class GoodThings: NSObject, NSCoding {
+    
+    //MARK: Properties
+    
+    let dateTime: Date
+    let goodThingsList: Array<String>
+    
+    //MARK: Archiving Paths
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("goodthings_data")
+    
+    //MARK: Types
+    
+    struct PropertyKey {
+        static let dateTime = "dateTime"
+        static let goodThingsList = "goodThingsList"
+    }
+    
+    //MARK: Initialization
+    
+    init?(dateTime: Date, goodThingsList: Array<String>) {
+        
+        // Initialize stored properties.
+        self.dateTime = dateTime
+        self.goodThingsList = goodThingsList
+        
+    }
+    
+    //MARK: NSCoding
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(dateTime, forKey: PropertyKey.dateTime)
+        aCoder.encode(goodThingsList, forKey: PropertyKey.goodThingsList)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        
+        // The date is required. If we cannot decode a dateTime Date, the initializer should fail.
+        guard let dateTime = aDecoder.decodeObject(forKey: PropertyKey.dateTime) as? Date else {
+            os_log("Unable to decode the date for a good thing object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        // The goodThingsList is required. If we cannot decode a goodThingsList [String], the initializer should fail.
+        guard let goodThingsList = aDecoder.decodeObject(forKey: PropertyKey.goodThingsList) as? [String] else {
+            os_log("Unable to decode the list of good things for a good thing object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        // Must call designated initializer.
+        self.init(dateTime: dateTime, goodThingsList: goodThingsList)
+    }
+    
+}
